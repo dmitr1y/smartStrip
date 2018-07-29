5bce9b5a2343dd7d1ce602e73aad806ca5b8d8fd
Dmitriy <dmitriy@konstantinov.com.ru>
2018-07-29 13:02:07 +0300

    fixed init modes

--- smartStrip.ino
+++ smartStrip.ino
@@ -62,21 +62,22 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("------> loop");
+  // Serial.println("------> loop");
   // digitalWrite(6, LOW);
-  if (Serial.available())
-  {
-  	/* code */
-  	ledMode=Serial.parseInt();
-  	Serial.print("readed from Serial: ");
-  	Serial.println(ledMode);
-  	delay(10);
-  }
+  // if (Serial.available())
+  // {
+  // 	/* code */
+  // 	ledMode=Serial.parseInt();
+  // 	Serial.print("readed from Serial: ");
+  // 	Serial.println(ledMode);
+  // 	delay(10);
+  // }
   if (BlueTooth.available() > 4) // если что то прислали
   {
     Serial.println("Data recieved!");
     recieveData();
     // delay(1);
+    initLedMods();
   }
   // while (BlueTooth.available()<5)
   ledMods();
@@ -87,14 +88,115 @@ void loop() {
   // }
 }
 
-void interruptFunc() {
-  Serial.println("interrupt!!!");
-  if (BlueTooth.available() > 4) {
-    Serial.println("    |-> Bluetooth available!");
-    recieveData();
-    Serial.println("    |-> turn on strip");
-    ledMods();
+// void interruptFunc() {
+//   Serial.println("interrupt!!!");
+//   if (BlueTooth.available() > 4) {
+//     Serial.println("    |-> Bluetooth available!");
+//     recieveData();
+//     Serial.println("    |-> turn on strip");
+//     ledMods();
+//   }
+// }
+
+void initLedMods() {
+  switch (ledMode) {
+  case 2:
+    thisdelay = 20;
+    break; // плавная смена цветов всей ленты
+  case 4:
+    thisdelay = 20;
+    thisstep = 10;
+    break; // случайная смена цветов
+  case 5:
+    thisdelay = 20;
+    thishue = 0;
+    break; // бегающий светодиод
+  case 6:
+    thisdelay = 40;
+    thishue = 0;
+    break; // бегающий паровозик светодиодов
+  case 7:
+    thisdelay = 40;
+    thishue = 0;
+    break; // вращаются красный и синий
+  case 8:
+    thisdelay = 40;
+    thishue = 0;
+    break; // вращается половина красных и половина синих
+  case 10:
+    thisdelay = 15;
+    thishue = 0;
+    break; // пульсация одним цветом
+  case 11:
+    thisdelay = 15;
+    thishue = 0;
+    break; // пульсация со сменой цветов
+  case 16:
+    thisdelay = 60;
+    thishue = 95;
+    break; // пульсирует значок радиации
+  case 20:
+    thisdelay = 100;
+    thishue = 0;
+    break; // красные вспышки спускаются вниз
+  case 21:
+    thisdelay = 100;
+    thishue = 180;
+    break; // полумесяц
+  case 25:
+    thisdelay = 35;
+    break; // безумие случайных вспышек
+  case 26:
+    thisdelay = 25;
+    thishue = 0;
+    break; // полицейская мигалка
+  case 27:
+    thisdelay = 25;
+    thishue = 0;
+    break; // RGB пропеллер
+  case 29:
+    thisdelay = 50;
+    thishue = 95;
+    break; // зелёненькие бегают по кругу случайно
+  case 30:
+    thisdelay = 5;
+    break; // крутая плавная вращающаяся радуга
+  case 33:
+    thisdelay = 50;
+    break; // плавное заполнение цветом
+  case 34:
+    thisdelay = 50;
+    break; // бегающие светодиоды
+  case 35:
+    thisdelay = 15;
+    break; // линейный огонь
+  case 37:
+    thisdelay = 20;
+    break; // очень плавная вращающаяся радуга
+  case 38:
+    thisdelay = 10;
+    break; // случайные разноцветные включения (1 - танцуют все, 0 -
+           // случайный 1 диод)
+  case 39:
+    thisdelay = 50;
+    break; // бегущие огни
+  case 41:
+    thisdelay = 20;
+    break; // случайные вспышки белого цвета на белом фоне
+  case 42:
+    thisdelay = 50;
+    break; // бегущие каждые 3 (ЧИСЛО СВЕТОДИОДОВ ДОЛЖНО БЫТЬ НЕЧЁТНОЕ)
+  case 43:
+    thisdelay = 50;
+    break; // бегущие каждые 3 радуга (ЧИСЛО СВЕТОДИОДОВ ДОЛЖНО БЫТЬ КРАТНО
+           // 3)
+  case 44:
+    thisdelay = 100;
+    break; // стробоскоп
+  default:
+    break;
   }
+  bouncedirection = 0;
 }
 
 void ledMods() {
@@ -108,129 +210,90 @@ void ledMods() {
     LEDS.show();
     break; //---ALL OFF                       // пауза
   case 2:
-    thisdelay = 20;
     rainbow_fade();
     break; // плавная смена цветов всей ленты
   case 4:
-    thisdelay = 20;
-    thisstep = 10;
     random_burst();
     break; // случайная смена цветов
   case 5:
-    thisdelay = 20;
-    thishue = 0;
     color_bounce();
     break; // бегающий светодиод
   case 6:
-    thisdelay = 40;
-    thishue = 0;
     color_bounceFADE();
     break; // бегающий паровозик светодиодов
   case 7:
-    thisdelay = 40;
-    thishue = 0;
     ems_lightsONE();
     break; // вращаются красный и синий
   case 8:
-    thisdelay = 40;
-    thishue = 0;
     ems_lightsALL();
     break; // вращается половина красных и половина синих
   case 10:
-    thisdelay = 15;
-    thishue = 0;
     pulse_one_color_all();
     break; // пульсация одним цветом
   case 11:
-    thisdelay = 15;
-    thishue = 0;
     pulse_one_color_all_rev();
     break; // пульсация со сменой цветов
   case 16:
-    thisdelay = 60;
-    thishue = 95;
     radiation();
     break; // пульсирует значок радиации
   case 18:
     white_temps();
     break; // бело синий градиент (?)
   case 20:
-    thisdelay = 100;
-    thishue = 0;
     pop_horizontal();
     break; // красные вспышки спускаются вниз
   case 21:
-    thisdelay = 100;
-    thishue = 180;
     quad_bright_curve();
     break; // полумесяц
   case 22:
     flame();
     break; // эффект пламени
   case 25:
-    thisdelay = 35;
     random_color_pop();
     break; // безумие случайных вспышек
   case 26:
-    thisdelay = 25;
-    thishue = 0;
     ems_lightsSTROBE();
     break; // полицейская мигалка
   case 27:
-    thisdelay = 25;
-    thishue = 0;
     rgb_propeller();
     break; // RGB пропеллер
   case 29:
-    thisdelay = 50;
-    thishue = 95;
     matrix();
     break; // зелёненькие бегают по кругу случайно
   case 30:
-    thisdelay = 5;
     new_rainbow_loop();
     break; // крутая плавная вращающаяся радуга
   case 33:
-    thisdelay = 50;
     colorWipe(0x00, 0xff, 0x00, thisdelay);
     colorWipe(0x00, 0x00, 0x00, thisdelay);
     break; // плавное заполнение цветом
   case 34:
-    thisdelay = 50;
     CylonBounce(0xff, 0, 0, 4, 10, thisdelay);
     break; // бегающие светодиоды
   case 35:
-    thisdelay = 15;
     Fire(55, 120, thisdelay);
     break; // линейный огонь
   case 37:
-    thisdelay = 20;
     rainbowCycle(thisdelay);
     break; // очень плавная вращающаяся радуга
   case 38:
-    thisdelay = 10;
     TwinkleRandom(20, thisdelay, 1);
     break; // случайные разноцветные включения (1 - танцуют все, 0 -
            // случайный 1 диод)
   case 39:
-    thisdelay = 50;
     RunningLights(0xff, 0xff, 0x00, thisdelay);
     break; // бегущие огни
   case 41:
-    thisdelay = 20;
     SnowSparkle(0x10, 0x10, 0x10, thisdelay, random(100, 1000));
     break; // случайные вспышки белого цвета на белом фоне
   case 42:
-    thisdelay = 50;
     theaterChase(0xff, 0, 0, thisdelay);
     break; // бегущие каждые 3 (ЧИСЛО СВЕТОДИОДОВ ДОЛЖНО БЫТЬ НЕЧЁТНОЕ)
   case 43:
-    thisdelay = 50;
     theaterChaseRainbow(thisdelay);
     break; // бегущие каждые 3 радуга (ЧИСЛО СВЕТОДИОДОВ ДОЛЖНО БЫТЬ КРАТНО
            // 3)
   case 44:
-    thisdelay = 100;
     Strobe(0xff, 0xff, 0xff, 10, thisdelay, 1000);
     break; // стробоскоп
   case 45:
@@ -245,7 +308,7 @@ void ledMods() {
   default:
     break;
   }
-  bouncedirection = 0;
+  // bouncedirection = 0;
 }
 
 void recieveData() {
